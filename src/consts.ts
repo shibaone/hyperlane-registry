5a94c7e0535ade6940e126617aff93f1444b9026
rome314-shiba <ramil.askarov@shib.io>
2025-05-13 18:59:03 +0300

    built registry

--- src/consts.ts
+++ src/consts.ts
@@ -1,6 +1,7 @@
 export const SCHEMA_REF = '# yaml-language-server: $schema=../schema.json';
 
-export const DEFAULT_GITHUB_REGISTRY = 'https://github.com/hyperlane-xyz/hyperlane-registry';
+// export const DEFAULT_GITHUB_REGISTRY = 'https://github.com/hyperlane-xyz/hyperlane-registry';
+export const DEFAULT_GITHUB_REGISTRY = 'https://github.com/shibaone/hyperlane-registry';
 export const GITHUB_FETCH_CONCURRENCY_LIMIT = 5;
 
 export const CHAIN_FILE_REGEX = /chains\/([a-z0-9]+)\/([a-z]+)\.(yaml|svg)/;
